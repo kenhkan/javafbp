7ba8906e8ff0804b5736b90a24dfdacbec36759f
Kenneth Kan <kenhkan@gmail.com>
2015-12-20 17:55:33 -0500

    Collate three

--- src/main/java/com/jpmorrsn/fbp/examples/networks/Update.java
+++ src/main/java/com/jpmorrsn/fbp/examples/networks/Update.java
@@ -22,15 +22,18 @@ public class Update extends Network {
     //tracing = true;
     component("Read Master", ReadFile.class);
     component("Read Details", ReadFile.class);
+    component("Read A", ReadFile.class);
     component("Collate", Collate.class);
     component("Display", WriteToConsole.class);
     connect(component("Read Master"), port("OUT"), component("Collate"), port("IN[0]"));
     connect(component("Read Details"), port("OUT"), component("Collate"), port("IN[1]"));
+    connect(component("Read A"), port("OUT"), component("Collate"), port("IN[2]"));
     connect(component("Collate"), port("OUT"), component("Display"), port("IN"));
     initialize("testdata/mfile".replace("/", File.separator), component("Read Master"), port("SOURCE"));
     initialize("testdata/dfile".replace("/", File.separator), component("Read Details"), port("SOURCE"));
+    initialize("testdata/afile".replace("/", File.separator), component("Read A"), port("SOURCE"));
 
-    initialize("3, 2, 5", component("Collate"), port("CTLFIELDS"));
+    initialize("1, 1", component("Collate"), port("CTLFIELDS"));
 
   }
 
